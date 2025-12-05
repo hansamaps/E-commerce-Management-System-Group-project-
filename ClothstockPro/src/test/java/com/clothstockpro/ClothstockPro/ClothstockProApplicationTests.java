3fc07d32b0c72343d1df53ae752551f1effcf028
Sajani Diwyanjali Hendawitharana <diwyanjali789sajani@gmail.com>
2025-12-05 11:25:30 +0530

    Add my updated project files

---
+++ ClothstockPro/src/test/java/com/clothstockpro/ClothstockPro/ClothstockProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clothstockpro.ClothstockPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothstockProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
