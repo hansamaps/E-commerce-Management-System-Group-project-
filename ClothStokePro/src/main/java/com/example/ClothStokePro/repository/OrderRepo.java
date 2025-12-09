0868f4538c6840bc10acd909f73a07589d4f2b9b
G.L.L Hansamali <lakshikahansamali92@gmail.com>
2025-12-09 20:19:30 +0530

    Delete ClothStokePro directory

--- ClothStokePro/src/main/java/com/example/ClothStokePro/repository/OrderRepo.java
+++
@@ -1,5 +0,0 @@
-package com.example.ClothStokePro.repository;
-
-public interface OrderRepo {
-
-}
--- ClothStokePro/src/main/java/com/example/ClothStokePro/service/ProductService.java
+++
@@ -1,5 +0,0 @@
-package com.example.ClothStokePro.service;
-
-public interface ProductService {
-
-}
