8b7cdd568c1d5c0c874fc82fbc6cdef3870acaa3
Sajani Diwyanjali Hendawitharana <diwyanjali789sajani@gmail.com>
2025-12-12 21:28:17 +0530

    add all the changes

--- Backend/ClothStokePro/src/main/java/com/example/ClothStokePro/repository/UserRepo.java
+++ Backend/ClothStokePro/src/main/java/com/example/ClothStokePro/repository/UserRepo.java
@@ -1,5 +1,12 @@
 package com.example.ClothStokePro.repository;
 
-public interface UserRepo {
+import com.example.ClothStokePro.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
+
