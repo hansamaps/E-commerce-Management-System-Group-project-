bc24089ba4a271212f5cdb10ca179e305b82421e
Inushi <inushikashmira20010@gmail.com>
2025-12-12 22:57:03 -0800

    add updated

--- src/main/java/com/example/ClothStokePro/repository/ProductRepo.java
+++ src/main/java/com/example/ClothStokePro/repository/ProductRepo.java
@@ -1,5 +1,11 @@
 package com.example.ClothStokePro.repository;
 
-public interface ProductRepo {
+import com.example.ClothStokePro.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+public interface ProductRepo extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryId(Long categoryId);
+    List<Product> findByNameContainingOrDescriptionContaining(String name, String description);
 }
