ca489c8e3103a73231bc9eca72830ca9020fcd20
hansamaps <lakshikahansamali92@gmail.com>
2025-12-13 20:02:46 +0530

    add orderitemservice.java

--- Backend/ClothStokePro/src/main/java/com/example/ClothStokePro/service/interf/OrderitemService.java
+++ Backend/ClothStokePro/src/main/java/com/example/ClothStokePro/service/interf/OrderitemService.java
@@ -1,5 +1,17 @@
 package com.example.ClothStokePro.service.interf;
 
-public class OrderitemService {
-    
+import com.example.ClothStokePro.dto.OrderRequest;
+import com.example.ClothStokePro.dto.Response;
+import com.example.ClothStokePro.enums.OrderStatus;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+
+public interface OrderitemService {
+    Response placeOrder(OrderRequest orderRequest);
+
+    Response updateOrderItemStatus(Long orderItemId, String status);
+
+    Response filterOrderItems(OrderStatus status, LocalDateTime startDate, LocalDateTime endDate, Long itemId,
+            Pageable pageable);
 }
