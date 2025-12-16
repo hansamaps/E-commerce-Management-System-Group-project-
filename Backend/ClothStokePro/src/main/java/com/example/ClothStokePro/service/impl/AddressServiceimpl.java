9857313b68c36861a7797e33ffcc61ef7de0ca87
hansamaps <lakshikahansamali92@gmail.com>
2025-12-16 13:21:17 +0530

    add all codes

--- Backend/ClothStokePro/src/main/java/com/example/ClothStokePro/service/impl/AddressServiceimpl.java
+++ Backend/ClothStokePro/src/main/java/com/example/ClothStokePro/service/impl/AddressServiceimpl.java
@@ -1,5 +1,48 @@
 package com.example.ClothStokePro.service.impl;
 
-public class AddressServiceimpl {
+import com.example.ClothStokePro.dto.AddressDto;
+import com.example.ClothStokePro.dto.Response;
+import com.example.ClothStokePro.entity.Address;
+import com.example.ClothStokePro.entity.User;
+import com.example.ClothStokePro.repository.AddressRepo;
+import com.example.ClothStokePro.service.interf.AddressService;
+import com.example.ClothStokePro.service.interf.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+public class AddressServiceimpl implements AddressService {
+
+    private final AddressRepo addressRepo;
+    private final UserService userService;
+
+    @Override
+    public Response saveAndUpdateAddress(AddressDto addressDto) {
+        User user = userService.getLoginUser();
+        Address address = user.getAddress();
+
+        if (address == null) {
+            address = new Address();
+            address.setUser(user);
+        }
+        if (addressDto.getStreet() != null)
+            address.setStreet(addressDto.getStreet());
+        if (addressDto.getCity() != null)
+            address.setCity(addressDto.getCity());
+        if (addressDto.getState() != null)
+            address.setState(addressDto.getState());
+        if (addressDto.getZipCode() != null)
+            address.setZipCode(addressDto.getZipCode());
+        if (addressDto.getCountry() != null)
+            address.setCountry(addressDto.getCountry());
+
+        addressRepo.save(address);
+
+        String message = (user.getAddress() == null) ? "Address successfully created" : "Address successfully updated";
+        return Response.builder()
+                .status(200)
+                .message(message)
+                .build();
+    }
 }
--- Backend/ClothStokePro/src/main/java/com/example/ClothStokePro/specification/OrderitemSpecification.java
+++ Backend/ClothStokePro/src/main/java/com/example/ClothStokePro/specification/OrderitemSpecification.java
@@ -10,31 +10,31 @@ import java.time.LocalDateTime;
 @SuppressWarnings("unused")
 public class OrderitemSpecification {
 
-    /**Specification to filter order items by status*/
-    public static Specification<Orderitem> hasStatus(OrderStatus status){
-        return ((root, query, criteriaBuilder) ->
-                status != null ? criteriaBuilder.equal(root.get("status"), status) : null);
+    /** Specification to filter order items by status */
+    public static Specification<Orderitem> hasStatus(OrderStatus status) {
+        return ((root, query, criteriaBuilder) -> status != null ? criteriaBuilder.equal(root.get("status"), status)
+                : null);
 
     }
 
-    /**Specification to filter order items by data range*/
-    public static Specification<Orderitem> createdBetween(LocalDateTime startDate, LocalDateTime endDate){
+    /** Specification to filter order items by data range */
+    public static Specification<Orderitem> createdBetween(LocalDateTime startDate, LocalDateTime endDate) {
         return ((root, query, criteriaBuilder) -> {
-            if (startDate != null && endDate != null){
+            if (startDate != null && endDate != null) {
                 return criteriaBuilder.between(root.get("createdAt"), startDate, endDate);
             } else if (startDate != null) {
                 return criteriaBuilder.greaterThanOrEqualTo(root.get("createdAt"), startDate);
             } else if (endDate != null) {
                 return criteriaBuilder.lessThanOrEqualTo(root.get("createdAt"), endDate);
-            }else{
+            } else {
                 return null;
             }
         });
     }
 
-    /** Generate specification to filter orderitems by item id*/
-    public static Specification<Orderitem> hasItemId(Long itemId){
-        return ((root, query, criteriaBuilder) ->
-                itemId != null ? criteriaBuilder.equal(root.get("id"), itemId) : null);
+    /** Generate specification to filter orderitems by item id */
+    public static Specification<Orderitem> hasItemId(Long itemId) {
+        return ((root, query, criteriaBuilder) -> itemId != null ? criteriaBuilder.equal(root.get("id"), itemId)
+                : null);
     }
 }
