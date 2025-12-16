e4f37e6aaa7ca06bbf0cdff4d5cd6f2f17d3b6b4
hansamaps <lakshikahansamali92@gmail.com>
2025-12-16 11:02:28 +0530

    add AddressController and OrderItemController

--- Backend/ClothStokePro/src/main/java/com/example/ClothStokePro/controller/AddressController.java
+++ Backend/ClothStokePro/src/main/java/com/example/ClothStokePro/controller/AddressController.java
@@ -1,5 +1,27 @@
 package com.example.ClothStokePro.controller;
 
+import com.example.ClothStokePro.dto.AddressDto;
+import com.example.ClothStokePro.dto.Response;
+import com.example.ClothStokePro.service.interf.AddressService;
+import lombok.RequiredArgsConstructor;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SuppressWarnings("unused")
+@RestController
+@RequestMapping("/address")
+@RequiredArgsConstructor
 public class AddressController {
 
+    private final AddressService addressService;
+
+    @PostMapping("/save")
+    public ResponseEntity<@Nullable Object> saveAndUpdateAddress(@RequestBody AddressDto addressDto) {
+        return ResponseEntity.ok(addressService.saveAndUpdateAddress(addressDto));
+    }
 }
--- Backend/ClothStokePro/src/main/java/com/example/ClothStokePro/service/interf/AddressService.java
+++ Backend/ClothStokePro/src/main/java/com/example/ClothStokePro/service/interf/AddressService.java
@@ -1,5 +1,12 @@
 package com.example.ClothStokePro.service.interf;
 
+import org.jspecify.annotations.Nullable;
+
+import com.example.ClothStokePro.dto.AddressDto;
+
 public interface AddressService {
+
+    @Nullable
+    Object saveAndUpdateAddress(AddressDto addressDto);
 
 }
